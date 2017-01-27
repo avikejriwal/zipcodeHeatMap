89c2c7913422ede9f78d0ad6b3f97b394147e2e4
avikejriwal <avi.kejriwal@gmail.com>
2017-01-26 23:54:02 -0600

    core file

---
+++ heatm.py
@@ -0,0 +1,59 @@
+import zipcode as zp #fast but had a lot of misses
+import pandas as pd
+import gmplot
+from pypostalcode import PostalCodeDatabase
+from pyzipcode import ZipCodeDatabase
+
+#parse the data into readable latitude/longitude
+dat = pd.read_csv('insert data file here')
+
+#parse appropriately
+usCodes = dat[dat['Country'] == 'United States of America']
+usCodes.dropna(subset = ['Zip'])
+usCodes = usCodes['Zip']
+
+#parse Canada data
+canCodes = dat[dat['Country'] == 'Canada']
+canCodes.dropna(subset=['Zip'])
+canCodes = canCodes['Zip']
+
+lat = []
+lng = []
+count = 0
+
+zcdb = ZipCodeDatabase()
+
+for z in usCodes:
+    r = z[:5]
+    while len(r) < 5:
+        r = '0' + r
+    try:
+        myZ = zp.isequal(str(r))
+        myDict = myZ.to_dict()
+        lat.append(myDict['lat'])
+        lng.append(myDict['lon'])
+    except:
+        try:
+            zC = zcdb[r]
+            lat.append(zC.latitude)
+            lng.append(zC.longitude)
+        except:
+            count +=1
+
+pcdb = PostalCodeDatabase()
+
+for z in canCodes:
+    r =  z[:3].upper()
+    try:
+        loc = pcdb[r]
+        lat.append(loc.latitude)
+        lng.append(loc.longitude)
+    except:
+        count +=1 #only like 3 zipcodes
+
+print count #number of misses
+
+#plot on Google Maps
+gmap = gmplot.GoogleMapPlotter(39.0558, -84.311, 20)
+gmap.heatmap(lat, lng)
+gmap.draw('mymap.html')
